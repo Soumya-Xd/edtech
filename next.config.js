7bf6f791439ef758eaefa612cd7986714be8c490
Soumya-Xd <Soumyasingharoy06@gmail.com>
2025-08-08 00:06:52 +0530

    Add config to ignore lint and type errors on build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // ⛔ Prevent ESLint errors from failing the Vercel build
+    // Prevent ESLint errors from failing the Vercel build
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // ⛔ Prevent type errors from failing the Vercel build
+    // Prevent type errors from failing the Vercel build
     ignoreBuildErrors: true,
   },
 };
